d7febac94d1b2f5f12e394fc564795f009e9b7ad
Axcis380 <133226284+Axcis380@users.noreply.github.com>
2023-11-29 19:47:06 +0100

    11-binary_tree_size.c

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,11 +1,12 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_size - measures the size of a binary tree
- * @tree: pointer to the root node of the tree to measure the size of
+ * binary_tree_size - measure the sie of a binary tree
+ * @tre: pointer to the ro node of the tre to measure the size of
  *
- * Return: size of a binary tree
+ * Return: sie of a bina tree
  */
+
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	size_t left, right;

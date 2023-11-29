f568f7f74a659fcd57a4e55ae79658ffe838a8f2
Axcis380 <133226284+Axcis380@users.noreply.github.com>
2023-11-29 22:01:49 +0100

    8-binary_tree_postorder.c

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,12 +1,12 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_postorder - goes through a binary tree using postorder traversal
- * @tree: pointer to the root node of the tree to traverse
- * @func: pointer to a function to call for each node.
- * The value in the node must be passed as a parameter to this function
+ * b__a__y_tr__e_p_st_rd_r - g__s thr__gh _ b__n__y tr__ _s_ng p_st_rd_r tr_v_rs_l
+ * @_r__ : pnt_r t_ th_ r_t n__e _f th_ tr__ t_ tr_v_rs_
+ * @_f_nc: p_ntr t fnct__n t_ c_ll f_r __ch n_d_.
+ * Th_ v_l_ _n th_ n_d_ m_st b_ p_ss_d _s _ p_r_m_t_r t_ th_s f_nct__n
  *
- * Return: void
+ * R__urn: v__d
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {

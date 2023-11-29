58d38f1660dd25b0c3ee2313ac401b6a78659968
Axcis380 <133226284+Axcis380@users.noreply.github.com>
2023-11-29 20:17:34 +0100

    13-binary_tree_nodes.c

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,10 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_leaf - checks if a node is a leaf
- * @node: pointer to the node to check
+ * bin__y_tr__ _s_l__f - ch__ks _f _ n_d_ _s _ l__f
+ * @n__e: p__nt__ _ t_ th_ n__e t_ ch__k
  *
- * Return: 1 if node is a leaf, and 0 otherwise. If node is NULL, return 0
+ * R__urn: 1 _f n__e _s _ l__f, _nd 0 _th_rw__s_. _f n__e _s NULL, r__urn 0
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
@@ -14,12 +14,12 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 }
 
 /**
- * binary_tree_nodes - counts the nodes with at least 1 child in a binary tree
- * @tree: pointer to the root node of the tree to count the nodes in
+ * b__a__y_tr__e_n__es - c__u__ts t_e n__es w__h _t l__st 1 c__ld _n _ b__n__y tr__e
+ * @t__e: p__nt__ _o t_e _o__ no_e _f t_e tr__e t_ c__nt t_e n__es _n
  *
- * Description: A NULL pointer is not a node
+ * D_scr__t__o_: _ N__L_ p__nt__r _s n_t _ n__e
  *
- * Return: number of nodes with at least 1 child in a binary tree
+ * R__urn: n_mb__r _f n__es w__h _t l__st 1 c__ld _n _ b__n__y tr__e
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {

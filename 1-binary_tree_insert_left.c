6a0c037cbc3533ac61b3143374dd5e4184acebc1
Axcis.380 <anas.baidouri@gmail.com>
2023-11-29 10:39:18 -0800

    updt 0 1 10 100

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -2,70 +2,47 @@
 #include <stdlib.h>
 
 /**
- * binary_tree_node_1 - Creates a binary tree node with a specified parent and value.
- * @parent: Pointer to the parent node.
- * @value: Value to be stored in the new node.
+ * binary_tree_node_1 - creates a binary tree node
+ * @parent: parnt of the node to create
+ * @value: value to store in new node
  *
- * Returns: Pointer to the new node.
+ * Return: pointer to the new node
  */
 binary_tree_t *binary_tree_node_1(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
-    if (new_node == NULL)
-        return (NULL);
+	binary_tree_t *new_node;
 
-    new_node->parent = parent;
-    new_node->n = value;
-    new_node->left = new_node->right = NULL;
-    return (new_node);
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->parent = parent;
+	new_node->n = value;
+	new_node->left = new_node->right = NULL;
+	return (new_node);
 }
 
 /**
- * binary_tree_insert_left - Inserts a new node as the left-child of a specified node.
- * @parent: Pointer to the node where the left-child will be inserted.
- * @value: Value to be stored in the new node.
+ * binary_tree_insert_left - inserts a node as the left-child of another node
+ * @parent: pointer to the node to insert the left-child in
+ * @value: value to store in the new node
  *
- * This function inserts a new node containing the given value as the left-child of
- * the specified parent node. If the parent already has a left-child, the new node
- * replaces it, and the existing left-child becomes the left-child of the new node.
+ * Description: If parent already has a left-child, the new node must take its
+ * place, and the old left-child must be set as the left-child of the new node.
  *
- * Returns: Pointer to the newly created node, or NULL if allocation fails.
+ * Return: pointer to the created node, or NULL on failure
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    if (parent == NULL)
-        return (NULL);
+	binary_tree_t *left_child;
 
-    binary_tree_t *left_child = binary_tree_node_1(parent, value);
-    if (left_child == NULL)
-        return (NULL);
-
-    left_child->left = parent->left;
-    if (left_child->left != NULL)
-        left_child->left->parent = left_child;
-
-    parent->left = left_child;
-    return (left_child);
-}
-
-/**
- * binary_tree_search - Searches for a value in a binary tree.
- * @tree: Pointer to the root node of the tree to search.
- * @value: Value to search for in the tree.
- *
- * Returns: Pointer to the node containing the found value, otherwise NULL.
- */
-binary_tree_t *binary_tree_search(const binary_tree_t *tree, int value)
-{
-    if (tree == NULL)
-        return (NULL);
-
-    if (tree->n == value)
-        return ((binary_tree_t *)tree);
-
-    binary_tree_t *left_result = binary_tree_search(tree->left, value);
-    if (left_result != NULL)
-        return (left_result);
-
-    return binary_tree_search(tree->right, value);
+	if (parent == NULL)
+		return (NULL);
+	left_child = binary_tree_node_1(parent, value);
+	if (left_child == NULL)
+		return (NULL);
+	left_child->left = parent->left;
+	if (left_child->left != NULL)
+		left_child->left->parent = left_child;
+	parent->left = left_child;
+	return (left_child);
 }
--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,35 +1,30 @@
 #include "binary_trees.h"
 
 /**
- * binary_trees_ancestor - Finds the lowest common ancestor of two nodes.
- * @first: Pointer to the first node.
- * @second: Pointer to the second node.
- *
- * This function finds the lowest common ancestor node of two given nodes
- * in a binary tree. It takes pointers to the first and second nodes and
- * returns a pointer to their lowest common ancestor.
- *
- * Return: Pointer to the lowest common ancestor node of the two given nodes.
+ * binary_trees_ancestor - finds the lowest common ancestor of two nodes
+ * @first: pointer to the first node
+ * @second: pointer to the second node
+ * Return: pointer to the lowest common ancestor node of the two given nodes
  */
+
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
-                                     const binary_tree_t *second)
+				     const binary_tree_t *second)
 {
-    binary_tree_t *tmp;
+	binary_tree_t *tmp;
 
-    if (first == NULL || second == NULL)
-        return (NULL);
-
-    tmp = (binary_tree_t *)second;
-    while (first)
-    {
-        while (second)
-        {
-            if (first == second)
-                return ((binary_tree_t *)first);
-            second = second->parent;
-        }
-        second = tmp;
-        first = first->parent;
-    }
-    return (NULL);
+	if (first == NULL || second == NULL)
+		return (NULL);
+	tmp = (binary_tree_t *)second;
+	while (first)
+	{
+		while (second)
+		{
+			if (first == second)
+				return ((binary_tree_t *)first);
+			second = second->parent;
+		}
+		second = tmp;
+		first = first->parent;
+	}
+	return (NULL);
 }

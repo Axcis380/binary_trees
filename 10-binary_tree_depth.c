1532a7a2daa8dfc5bf6071dcc36c2daefb7d1ff3
Axcis380 <133226284+Axcis380@users.noreply.github.com>
2023-11-29 19:42:40 +0100

    10-binary_tree_depth.c

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,10 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_depth - measures the depth of a node in a binary tree
- * @node: pointer to the node to measure the depth of
+ * binary_tree_depth - measure the dpth of a nod n a binar tree
+ * @node: pointr to thenod o measure he dephof
  *
- * Return: depth of a node in a binary tree. If node is NULL, return 0
+ * Return: deph of a nde in a biny tre. If node is NLL, return 0
  */
 size_t binary_tree_depth(const binary_tree_t *node)
 {

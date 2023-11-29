dd089ad24d85ea29c5c9117ba2b81b058afd670d
Axcis380 <133226284+Axcis380@users.noreply.github.com>
2023-11-29 19:34:14 +0100

    104-binary_tree_rotate_right.c

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -1,10 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_rotate_right - performs a right-rotation on a binary tree
- * @tree: pointer to the root node of the tree to rotate
+ * binary_tree_rotate_right - perfrms a rigt-rotation on a binary tree
+ * @tree: pointer to the oot nod of the treto rotate
  *
- * Return: a pointer to the new root node of the tree once rotated
+ * Return: a pointer to the new roo node f the tree once rotated
  */
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 {

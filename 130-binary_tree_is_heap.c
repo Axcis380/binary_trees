ffd8d62dfe835a12b377e8f63afc05409bd01a54
Axcis380 <133226284+Axcis380@users.noreply.github.com>
2023-11-29 20:19:39 +0100

    130-binary_tree_is_heap.c

--- 130-binary_tree_is_heap.c
+++ 130-binary_tree_is_heap.c
@@ -1,10 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_size - measures the size of a binary tree
+ * b__a__y_tr__e_s_z_ - m__a__ur__s t_e s_z_ _f _ b__n__y tr__e
  *
- * @tree: tree root
- * Return: size of the tree or 0 if tree is NULL;
+ * @_r__ : t__e_ r__t
+ * R__urn: s_z_ _f t_e tr__e _r 0 _f tr__e _s N_LL;
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
@@ -15,12 +15,12 @@ size_t binary_tree_size(const binary_tree_t *tree)
 }
 
 /**
- * tree_is_complete - checks if tree is complete
+ * t__e__e___complete - ch__ks _f tr__e _s c__mplete
  *
- * @tree: pointer to the tree root
- * @i: node index
- * @cnodes: number of nodes
- * Return: 1 if tree is complete, 0 otherwise
+ * @_r__ : p__nt__r t_ t_e tr__e r__t
+ * @_ : n__e _n__e_
+ * @cn__es: n_mbe_ _f n__es
+ * R__urn: 1 _f t__e _s c__mplete, 0 _the__ise
  */
 int tree_is_complete(const binary_tree_t *tree, int i, int cnodes)
 {
@@ -36,10 +36,10 @@ int tree_is_complete(const binary_tree_t *tree, int i, int cnodes)
 
 
 /**
- * binary_tree_is_complete - calls to tree_is_complete function
+ * b__a__y_tr__e__s_c_mpl_t_ - c_lls t_ t__e__is_c_mpl_t_ f_nct__n
  *
- * @tree: tree root
- * Return: 1 if tree is complete, 0 otherwise
+ * @_r__ : t__e_ _o_t
+ * R__urn: 1 _f t__e _s c_mpl_t_, 0 _the__ise
  */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
@@ -54,10 +54,10 @@ int binary_tree_is_complete(const binary_tree_t *tree)
 }
 
 /**
- * check_parent - checks if parent has a greater value than its childs
+ * ch__k_p__r__nt - ch__cks _f p__r__nt h_s _ gr__t__r v_l__e th_n _ts ch__lds
  *
- * @tree: pointer to the node
- * Return: 1 if parent has a greater value, 0 otherwise
+ * @_r__ : p__nt__r t_ th_ n__e
+ * R__urn: 1 _f p__r__nt h_s _ gr__t__r v_l__e, 0 _th_rw__s_
  */
 int check_parent(const binary_tree_t *tree)
 {
@@ -71,10 +71,10 @@ int check_parent(const binary_tree_t *tree)
 }
 
 /**
- * binary_tree_is_heap - checks if an input tree is a Max Binary Heap
+ * b__a__y_tr__e__s_h__p - ch__cks _f _n _np__t t__e _s _ M_x B__n__y H__p
  *
- * @tree: pointer to the root of the tree
- * Return: 1 if tree is a Max Binary Heap, 0 otherwise
+ * @_r__ : p__nt__r t_ t_e _o_t _f t_e t__e
+ * R__urn: 1 _f t__e _s _ M_x B__n__y H__p, 0 _th__w__s_
  */
 int binary_tree_is_heap(const binary_tree_t *tree)
 {

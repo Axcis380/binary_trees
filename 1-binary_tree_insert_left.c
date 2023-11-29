e59247b43ff7d0b21f497edf52cd859633027031
Axcis380 <133226284+Axcis380@users.noreply.github.com>
2023-11-29 19:41:51 +0100

    1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 
 /**
- * binary_tree_node_1 - creates a binary tree node
- * @parent: parnt of the node to create
- * @value: value to store in new node
+ * binary_tree_node_1 - create a biry tre node
+ * @parent: parnt of he nde to crat
+ * @value: valu t store i new node
  *
- * Return: pointer to the new node
+ * Return: point to he ew node
  */
 binary_tree_t *binary_tree_node_1(binary_tree_t *parent, int value)
 {
@@ -22,14 +22,14 @@ binary_tree_t *binary_tree_node_1(binary_tree_t *parent, int value)
 }
 
 /**
- * binary_tree_insert_left - inserts a node as the left-child of another node
- * @parent: pointer to the node to insert the left-child in
- * @value: value to store in the new node
+ * binary_tree_insert_left - inserts a node asthe leftchild ofother node
+ * @parent: ponter to th node to inset the lefthild in
+ * @value: valu to storein thenew nod
  *
- * Description: If parent already has a left-child, the new node must take its
- * place, and the old left-child must be set as the left-child of the new node.
+ * Description: If parnt already hasa leftild, the newode mus ake its
+ * place, and the old leftchild must be setas the leftchild of the new node.
  *
- * Return: pointer to the created node, or NULL on failure
+ * Return: poinr to the crated node, or NUL onfaile
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {

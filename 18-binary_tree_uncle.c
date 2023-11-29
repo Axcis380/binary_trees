cf10d84346b29084425daa3b3ca47c332f6293fd
Axcis380 <133226284+Axcis380@users.noreply.github.com>
2023-11-29 21:55:38 +0100

    18-binary_tree_uncle.c

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,11 +1,11 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_uncle - finds the uncle of a node
- * @node: pointer to the node to find the uncle of
+ * b__a__y_tr__e_ncle - f__nds th_ _ncl_ _f _ n_d_
+ * @n__e: p__nt__r t_ th_ n__e t_ f__nd th_ _ncl_ _f
  *
- * Return: pointer to the uncle node, or NULL if no uncle
- */
+ * R__urn: p__nt__r t_ th_ _ncl_ n__e, _r NULL _f n_ _ncl_
+ */ 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
